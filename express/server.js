85e0fb1a59118213319714812a2cdad68bdc8f03
KavyaRPatel <kavyarp197@gmail.com>
2023-01-09 17:09:45 +0530

    views using express

--- express/server.js
+++ express/server.js
@@ -1,8 +1,38 @@
+
 const express = require ("express")
-
 const app = express();
-app.get("/ping", (req,res)=>{
-    res.send("Node app")
-});
+const PORT = 3000;
 
-app.listen(3000, ()=>{console.log("Server started on port 3000")})
+// app.get("/ping", (req,res)=>{
+//     res.send("Node app")
+// });
+
+// app.listen(3000, ()=>{console.log("Server started on port 3000")})
+
+//routing
+// const data ={
+//     id:1,
+//     name:"India"
+// }
+
+// app.get('/', (req,res)=>{
+//     res.end("Welcome to home page");
+// })
+
+// app.get('/about', (req,res)=>{
+//     res.send("Welcome to about page");
+// })
+
+// app.get('/whether', (req,res)=>{
+//     res.send("Whether is nice");
+// })
+
+// app.listen(PORT,()=> {
+//     console.log("Server started on PORT:", PORT)
+// })
+
+//views
+app.set('view engine','pug')
+app.get('/', (req,res)=>{
+    res.render('index')
+}).listen(PORT, console.log("Server started on port http://localhost:3000"))
