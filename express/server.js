7b201d652bcfacda22fb24bbe1dbc25082e04083
KavyaRPatel <kavyarp197@gmail.com>
2023-01-10 10:25:56 +0530

    Forms using express

--- express/server.js
+++ express/server.js
@@ -1,5 +1,5 @@
 
-const express = require ("express")
+const express = require("express")
 const app = express();
 const PORT = 3000;
 
@@ -32,7 +32,26 @@ const PORT = 3000;
 // })
 
 //views
-app.set('view engine','pug')
-app.get('/', (req,res)=>{
-    res.render('index')
-}).listen(PORT, console.log("Server started on port http://localhost:3000"))
+// app.set('view engine','pug')
+// app.get('/', (req,res)=>{
+//     res.render('index')
+// }).listen(PORT, console.log("Server started on port http://localhost:3000"))
+
+// Forms using Express 
+
+app.set('view engine', 'pug')
+
+app.use(express.urlencoded({
+    extended: true
+
+}))
+
+app.get('/', (req, res) => {
+    res.render('index', { title: "Form Handling" })
+})
+app.post("/form_submit", (req, res) => {
+    const username = req.body.username
+    const email = req.body.email
+    res.end(`Username:${username} and Email: ${email}`);
+})
+app.listen(PORT, ()=> {console.log("Server started on port http://localhost:3000")})
