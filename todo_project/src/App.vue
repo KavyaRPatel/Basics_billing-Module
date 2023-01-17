585a9bd67f4fb2ce6e2d5ccb50d60e8d4f5e46bb
KavyaRPatel <kavyarp197@gmail.com>
2023-01-17 18:43:53 +0530

    Header Done

--- todo_project/src/App.vue
+++ todo_project/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <app-header v-bind:title="title"></app-header>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import header from './components/Header.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    'appHeader': header 
+  },
+  data() {
+    return {
+        todos: [
+            { id: 1, name: 'Kavya', task: 'Make a todo app' },
+            { id: 2, name: 'Ravi', task: 'Comlete node tutorial' }
+        ]
+    }
+}
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ todo_project/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return{
+      title: "Todo List"
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color: lightblue;
+  padding: 10px;
+}
+
+h1 {
+  color: peru;
+  text-align: center;
+}
+</style>
