233536a0971006bec5e24b3f10d82821a88da85b
KavyaRPatel <kavyarp197@gmail.com>
2023-01-17 18:50:53 +0530

    Data added

--- todo_project/src/App.vue
+++ todo_project/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <app-header v-bind:title="title"></app-header>
+  <app-main v-bind:todo="todos"></app-main>
 
 </template>
 
 <script>
 import header from './components/Header.vue';
+import main from './components/main.vue';
+
 export default {
   components: {
-    'appHeader': header 
+    'app-header': header,
+    'app-main': main
+
   },
   data() {
     return {
         todos: [
             { id: 1, name: 'Kavya', task: 'Make a todo app' },
-            { id: 2, name: 'Ravi', task: 'Comlete node tutorial' }
+            { id: 2, name: 'Ravi', task: 'Complete node tutorial' }
         ]
     }
 }
---
+++ todo_project/src/components/main.vue
@@ -0,0 +1,23 @@
+<template>
+
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    data() {
+      return{
+        todos: [
+            { id: 1, name: 'Kavya', task: 'Make a todo app' },
+            { id: 2, name: 'Ravi', task: 'Complete node tutorial' }
+        ]  
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
